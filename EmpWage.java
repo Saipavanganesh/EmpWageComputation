ff145ccec5a5926462059bfc7a34ed24bbba4d42
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-13 02:09:05 +0530

    Calculated total sal for 20 days based on hours

--- EmpWage.java
+++ EmpWage.java
@@ -4,28 +4,41 @@ import java.lang.Math;
 public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        System.out.println("********************************************");
 
-        double empCheck=Math.floor(Math.random()*10)%3;
         int wagePerHour=20;
-        int fullDayHours=8;
+        int fullDayHours=0;
+        int totalWorkingDays=20;
+        int totalHoursWorked=0;
+        int daysPresent=0;
         int salary;
 
-
-        switch (Double.toString(empCheck)){
-            case "1.0":
-                System.out.println("Employee is present");
-                break;
-            case "2.0":
-                System.out.println("Emp is Part time present");
-                fullDayHours=4;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                fullDayHours=0;
-                break;
+        for(int i=0;i<totalWorkingDays;i++){
+            double empCheck=Math.floor(Math.random()*10)%3;
+            switch (Double.toString(empCheck)){
+                case "1.0":
+                    //System.out.println("Employee is present");
+                    daysPresent++;
+                    fullDayHours=8;
+                    totalHoursWorked = totalHoursWorked+fullDayHours;
+                    break;
+                case "2.0":
+                    //System.out.println("Emp is Part time present");
+                    fullDayHours=4;
+                    totalHoursWorked = totalHoursWorked+fullDayHours;
+                    daysPresent++;
+                    break;
+                default:
+                    //System.out.println("Employee is absent");
+                    fullDayHours=0;
+                    totalHoursWorked = totalHoursWorked+fullDayHours;
+                    break;
+            }
         }
 
-        salary=fullDayHours*wagePerHour;
-        System.out.println("Daily Wage is "+salary);
+        salary=totalHoursWorked*wagePerHour;
+        System.out.println("No.of days employee worked: "+daysPresent);
+        System.out.println("Total Hours="+totalHoursWorked);
+        System.out.println("Total Salary is "+salary);
     }
 }
